520e53919843fa7137503f8a509a8230e719a85c
gnikit <giannis.nikiteas@gmail.com>
2025-05-31 09:45:04 +0100

    ci(debug): trying to get macos13 to work

--- tools/wheels/cibw_before_build_macos.sh
+++ tools/wheels/cibw_before_build_macos.sh
@@ -4,26 +4,32 @@ PROJECT_DIR="$1"
 
 if [[ "$(uname -m)" == "x86_64" ]]; then
     export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-10.9}
+    GFOR_ARCH="x86_64"
 else
     export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-11.0}
+    GFOR_ARCH="arm64"
 fi
 
 if [ -n "$GITHUB_ENV" ]; then
     brew unlink gcc || true
 fi
 
+echo "@@@ MACOSX_DEPLOYMENT_TARGET:" $MACOSX_DEPLOYMENT_TARGET
+echo "@@@ GFOR_ARCH:" $GFOR_ARCH
+# ---------- install gfortran ----------
+
 source $PROJECT_DIR/tools/wheels/gfortran_utils.sh
 install_gfortran
 
-eval "$(gfortran_get_vars)"
-{
-    echo "FC=$FC"
-    echo "F90=$FC"
-    echo "F95=$FC"
-    echo "LDFLAGS=$LDFLAGS"
-    echo "CFLAGS=-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-    echo "FFLAGS=$CFLAGS"
-} >>"$GITHUB_ENV"
+# ---------- export compiler + rpath for subsequent CMake & delocate ----------
+GFOR_ROOT="/opt/gfortran-darwin-${GFOR_ARCH}-native"
+echo "FC=${GFOR_ROOT}/bin/gfortran" >>"$GITHUB_ENV"
+echo "LDFLAGS=-L${GFOR_ROOT}/lib -Wl,-rpath,${GFOR_ROOT}/lib -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}" >>"$GITHUB_ENV"
+echo "F90=${GFOR_ROOT}/bin/gfortran" >>"$GITHUB_ENV"
+echo "F95=${GFOR_ROOT}/bin/gfortran" >>"$GITHUB_ENV"
+
+echo "@@@ FC:" "${GFOR_ROOT}/bin/gfortran"
+echo "@@@ LDFLAGS:" "-L${GFOR_ROOT}/lib -Wl,-rpath,${GFOR_ROOT}/lib"
 
 # Cannot pass these variables to the wheel building process since before-build
 # executes in a subprocess. So we print them out and manually set them in
